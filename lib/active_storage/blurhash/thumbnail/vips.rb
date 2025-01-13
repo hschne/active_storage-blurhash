fc95ad480fe6371fe0ba6ed1814cd9622786c2b9
Henrik Hauge Bjørnskov <19725+henrikbjorn@users.noreply.github.com>
2025-01-13 12:28:16 +0100

    Use `to_a` instead of memory unpack and check number of bands

--- lib/active_storage/blurhash/thumbnail/vips.rb
+++ lib/active_storage/blurhash/thumbnail/vips.rb
@@ -8,10 +8,21 @@ module ActiveStorage
           @thumbnail = ::Vips::Image.new_from_file(
             ::ImageProcessing::Vips.source(image.filename).resize_to_limit(200, 200).call.path
           )
+
+          @thumbnail = case @thumbnail.bands
+          when 1
+            @thumbnail.bandjoin(Array.new(3 - @thumbnail.bands, @thumbnail))
+          when 2
+            @thumbnail.bandjoin(@thumbnail.extract_band(0))
+          when 3
+            @thumbnail
+          else
+            @thumnail.extract_band(0, n: 3)
+          end
         end
 
         def pixels
-          @thumbnail.write_to_memory.unpack("C*")
+          @thumbnail.to_a.flatten
         end
       end
     end
